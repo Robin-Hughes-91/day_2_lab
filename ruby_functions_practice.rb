25368482279762bb8f0ccc8f0bbd53fed84e5a3f
Robin Hughes <robin22391@hotmail.com>
2018-10-02 15:32:19 +0100

    lab stuff

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,31 +1,61 @@
-def return_10
-  return 10
-end
+# def return_10
+#   return 10
+# end
+#
+# def add(number_a, number_b)
+#   return number_a + number_b
+# end
+#
+# def subtract(number_a, number_b)
+#   return number_a - number_b
+# end
+#
+# def multiply(number_a, number_b)
+#   return number_a * number_b
+# end
+#
+# def divide(number_a, number_b)
+#   return number_a / number_b
+# end
+#
+# def length_of_string(string)
+#   return string.length
+# end
+#
+# def join_string(string_1, string_2)
+#   return string_1 + string_2
+# end
+#
+# def add_string_as_number(number_a, number_b)
+#   return number_a.to_i + number_b.to_i
+# end
+#
+# def number_to_full_month_name( number )
+#   case number
+#   when 3
+#     return "March"
+#   when 1
+#     return "January"
+#   when 9
+#     return "September"
+#   else
+#     return "fail"
+#   end
+# end
+#
+# def number_to_short_month_name( number )
+#   case number
+#   when 1
+#     return "Jan"
+#   when 4
+#     return "Apr"
+#   when 10
+#     return "Oct"
+#   else
+#     return "fail"
+#   end
+# end
 
-def add(number_a, number_b)
-  return number_a + number_b
-end
-
-def subtract(number_a, number_b)
-  return number_a - number_b
-end
-
-def multiply(number_a, number_b)
-  return number_a * number_b
-end
-
-def divide(number_a, number_b)
-  return number_a / number_b
-end
-
-def length_of_string(string)
-  return string.length
-end
-
-def join_string(string_1, string_2)
-  return string_1 + string_2
-end
-
-def add_string_as_number(number_a, number_b)
-  return number_a.to_i + number_b.to_i
+def volume_of_cube(side_length)
+  return side_length.to_i ** 3
 end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -3,7 +3,7 @@ require( 'minitest/rg' )
 require_relative( '../ruby_functions_practice' )
 
 class FunctionsTest < MiniTest::Test
-
+#
 #   def test_return_10()
 #     return_10_result = return_10()
 #     assert_equal( 10, return_10_result )
@@ -28,25 +28,25 @@ class FunctionsTest < MiniTest::Test
 #     divide_result = divide( 10, 2 )
 #     assert_equal( 5, divide_result )
 #   end
-
-  # def test_length_of_string()
-  #   test_string = "A string of length 21"
-  #   length_of_string = length_of_string( test_string )
-  #   assert_equal( 21, length_of_string )
-  # end
-
-  # def test_join_string()
-  #   string_1 = "Mary had a little lamb, "
-  #   string_2 = "its fleece was white as snow"
-  #   joined_string = join_string( string_1, string_2 )
-  #   assert_equal( "Mary had a little lamb, its fleece was white as snow", joined_string )
-  # end
 #
-  def test_add_string_as_number()
-    add_result = add_string_as_number( "1", "2" )
-    assert_equal( 3, add_result )
-  end
+#   def test_length_of_string()
+#     test_string = "A string of length 21"
+#     length_of_string = length_of_string( test_string )
+#     assert_equal( 21, length_of_string )
+#   end
 #
+#   def test_join_string()
+#     string_1 = "Mary had a little lamb, "
+#     string_2 = "its fleece was white as snow"
+#     joined_string = join_string( string_1, string_2 )
+#     assert_equal( "Mary had a little lamb, its fleece was white as snow", joined_string )
+#   end
+#
+#   def test_add_string_as_number()
+#     add_result = add_string_as_number( "1", "2" )
+#     assert_equal( 3, add_result )
+#   end
+# #
 #   def test_number_to_full_name__month_1()
 #     result = number_to_full_month_name( 1 )
 #     assert_equal( "January", result )
@@ -61,7 +61,7 @@ class FunctionsTest < MiniTest::Test
 #     result = number_to_full_month_name( 9 )
 #     assert_equal( "September", result )
 #   end
-#
+# #
 #   def test_substring__month_1()
 #     first_month_string = number_to_short_month_name( 1 )
 #     assert_equal( "Jan", first_month_string )
@@ -76,13 +76,15 @@ class FunctionsTest < MiniTest::Test
 #     tenth_month_string = number_to_short_month_name( 10 )
 #     assert_equal( "Oct", tenth_month_string )
 #   end
-#
-#   #Further
-#
-#   #Given the length of a side of a cube calculate the volume
-#   def test_volume_of_cube()
-#     #add test code here
-#   end
+# #
+# #   #Further
+
+  #Given the length of a side of a cube calculate the volume
+  def test_volume_of_cube()
+    #add test code here
+    side_length = given_side_length( 3 )
+    assert_equal(27, volume_of_cube)
+  end
 #
 #   #Given the radius of a sphere calculate the volume
 #   def test_volume_of_sphere()
