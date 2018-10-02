5c01a232f4576abaaa7d7c44ffe2dbe0ad07bfbd
Robin Hughes <robin22391@hotmail.com>
2018-10-02 15:02:39 +0100

    adds 5 tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,31 @@
 def return_10
   return 10
-  end
+end
+
+def add(number_a, number_b)
+  return number_a + number_b
+end
+
+def subtract(number_a, number_b)
+  return number_a - number_b
+end
+
+def multiply(number_a, number_b)
+  return number_a * number_b
+end
+
+def divide(number_a, number_b)
+  return number_a / number_b
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(number_a, number_b)
+  return number_a.to_i + number_b.to_i
+end
